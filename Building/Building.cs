02db2359648763288f4cf8dcf9a0f1e840b00660
yulia_norkina <yulia.norkina2@gmail.com>
2019-10-14 11:11:23 +0300

    add class Buildings and implement some methods

---
+++ Building/Building.cs
@@ -0,0 +1,47 @@
+﻿namespace Building
+{
+    class Building
+    {
+        int id;
+        static int idGenerator = 0;
+        double height;
+        int numberOfStoreys;
+        int numberOfApartments;
+        int numberOfEntrances;
+
+        public Building(double height, int numberOfStoreys, int numberOfApartments, int numberOfEntrances)
+        {
+            id = generator();
+            this.height = height;
+            this.numberOfStoreys = numberOfStoreys;
+            this.numberOfApartments = numberOfApartments;
+            this.numberOfEntrances = numberOfEntrances;
+        }
+
+        public Building getBuildingInfo()
+        {
+            return this;
+        }
+
+        public double floorHeightCalculations()
+        {
+            return height / numberOfStoreys;
+        }
+
+        public int calculationNumberOfApartmentsInEntrance()
+        {
+
+            return numberOfApartments / numberOfEntrances;
+        }
+
+        public int calculationNumberOfApartmentsPerFloor()
+        {
+            return calculationNumberOfApartmentsInEntrance() / numberOfStoreys;
+        }
+
+        private int generator()
+        {
+            return ++idGenerator;
+        }
+    }
+}
